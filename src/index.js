12f08672fbeaa4e7e6d3d702a4df4856aed2e977
rene <renegmed@yahoo.com>
2018-12-07 07:56:59 -0500

    introducing jsx elements

--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom';
 
 const tasks = ['take out the trash', 'shovel the driveway', 'walk the dog'];
 
-const element = React.createElement('ol', null, 
-    tasks.map((task, index) => React.createElement('li', {key: index}, task)) 
-);
-
+// JSX elements
+const element = 
+    <div>
+        <h1> Task List </h1>
+        <ol>
+            { tasks.map((task, index) => <li key= {index}> {task} </li>) } 
+        </ol> 
+    </div>
+ 
 ReactDOM.render(element, document.getElementById('root'));
