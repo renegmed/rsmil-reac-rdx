3a7c25a26f485b03b46860b8045722f8d8baa5a4
rene <renegmed@yahoo.com>
2018-12-07 08:42:51 -0500

    refactor components - broken into files

---
+++ src/components/Main.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Title from './Title';
+import List from './List';
+
+class Main extends Component {
+    render() {
+        return <div>
+                 <Title title = {'Today\'s Task List'}/>
+                 <List tasks = {['Mow the lawn', 'walk the dog']}/>
+                 <List tasks = {['hose the driveway', 'finish the laundry']}/>
+               </div>
+        
+    }
+}
+
+export default Main;
--- src/index.js
+++ src/index.js
@@ -1,35 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-  
-class List extends Component {
-
-    // NOTE 'this' points to the component that is being rendered, i.e Main's List.
-    // 
-    render() {
-        return ( 
-        <ol>
-            { this.props.tasks.map((task, index) => <li key= {index}> {task} </li>) } 
-        </ol> 
-        )
-    }
-} 
-
-class Title extends Component {
-    render() {
-        return (    
-            <h1> {this.props.title} </h1>
-        )
-    }
-}
-
-class Main extends Component {
-    render() {
-        return <div>
-                 <Title title = {'Today\'s Task List'}/>
-                 <List tasks = {['Mow the lawn', 'walk the dog']}/>
-                 <List tasks = {['hose the driveway', 'finish the laundry']}/>
-               </div>
-        
-    }
-}
+import Main from './components/Main';
+   
 ReactDOM.render(<Main />, document.getElementById('root'));
