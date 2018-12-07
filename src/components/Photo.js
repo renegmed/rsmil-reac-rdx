36af279ec087a95a8f9a6ef2205f3ee4470ee77c
rene <renegmed@yahoo.com>
2018-12-07 17:28:59 -0500

    using Prop-Types, detecting and preventing missing prop errors

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function Photo(props){
     const post = props.post;
@@ -14,4 +15,8 @@ function Photo(props){
                </figure>
 } 
 
+Photo.propTypes = {
+    post: PropTypes.object.isRequired, 
+    onRemovePhoto: PropTypes.func.isRequired 
+}
 export default Photo;
