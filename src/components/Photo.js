3c19eaeb999be5d0453aa29d0a70cb342f0fbf56
rene <renegmed@yahoo.com>
2018-12-09 13:10:09 -0500

    revert back Photo component as connect is not recommended for swallow level

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
 
 function Photo(props){
-    console.log("Photo]", props.posts);
+     
     const post = props.post;
     return <figure className="figure">
                 <img className="photo" src={post.imageLink} alt={post.description} />
@@ -11,20 +10,13 @@ function Photo(props){
                 <div className="button-container">
                     <button onClick = {() => {
                         props.onRemovePhoto(post);
-                    }}> Remove </button>
+                        }}> Remove </button>
                 </div>
                     
                </figure>
 } 
 
-function mapStateToProps(state) {
-    return {
-        posts: state
-    }
-}
-
 Photo.propTypes = {
     post: PropTypes.object.isRequired,  
 }
-export default connect(mapStateToProps)(Photo);
-//export default Photo;
+export default Photo;
