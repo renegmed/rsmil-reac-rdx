e9860c7ba9d02155de043d617922c4f487846d3c
rene <renegmed@yahoo.com>
2018-12-09 00:37:49 -0500

    UI setup - added text input fields and buttons on AddPhoto

--- src/components/AddPhoto.js
+++ src/components/AddPhoto.js
@@ -2,7 +2,19 @@ import React, { Component } from 'react';
 
 class AddPhoto extends Component {
     render() {
-        return <h1> This is the page where we will add photos </h1>
+        return (
+            <div>
+                <h1> Photowall </h1>
+                <div className="form">
+                    <form>
+                        <input type="text" placeholder="Link"/>
+                        <input type="text" placeholder="Description"/>
+                        <button> Post </button>                        
+                    </form>
+                </div>
+            </div>
+        
+        )
     }
 }
 
