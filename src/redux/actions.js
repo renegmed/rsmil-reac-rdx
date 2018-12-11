383a247caf02a0546326bf71a2b2dfda7570a58e
rene <renegmed@yahoo.com>
2018-12-11 17:48:12 -0500

    Removing posts

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -22,6 +22,13 @@ export function startLoadingPosts() {
         });
     }
 }
+export function startRemovingPost(index, id) {
+    return (dispatch) => {
+        return database.ref(`posts/${id}`).remove().then( () => {
+            dispatch(removePost(index));
+        });
+    }
+}
 export function removePost(index) {
     console.log("[actions] removePost index ", index);
     return {
