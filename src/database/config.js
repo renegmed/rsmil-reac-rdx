f531d17d6dad162166431b994ada440554da90e9
rene <renegmed@yahoo.com>
2018-12-11 16:47:16 -0500

    firebase database setup, create db project in https://console.firebase.google.com

---
+++ src/database/config.js
@@ -0,0 +1,15 @@
+import * as firebase from 'firebase';
+var config = {
+    apiKey: "AIzaSyCreLsRx1RMSgjWo1UNs0TCTqrzhufmgzA",
+    authDomain: "photowall-8f82b.firebaseapp.com",
+    databaseURL: "https://photowall-8f82b.firebaseio.com",
+    projectId: "photowall-8f82b",
+    storageBucket: "photowall-8f82b.appspot.com",
+    messagingSenderId: "685209826891"
+};
+
+firebase.initializeApp(config)
+
+const database  = firebase.database();
+
+export default database;
